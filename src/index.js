571e8075676b60988cf9ff296cd3e056a4521fda
Zahid Hasan <jahidhasann67@gmail.com>
2021-07-10 20:48:34 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
+import Card from './Card';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+  <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+  <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  </div>
+  </React.StrictMode>,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
