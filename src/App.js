8af75b562c05b5b4f9b3a025375c97a99829839c
Zahid Hasan <jahidhasann67@gmail.com>
2021-07-13 14:18:22 +0600

    updated

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfeild: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchfeild: event.target.value })
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfeild.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+    
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
-import Card from './Card';
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { robots } from './robots';
 
-ReactDOM.render(
-  <React.StrictMode>
-  <div>
-  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-  <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-  <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-  </div>
-  </React.StrictMode>,document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
 reportWebVitals();
